8891be4ad226c8966dbf0c56e982127b9e224374
David Ku <dku7@proton.me>
2024-10-19 15:42:05 +0100

    render list from an object

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,46 @@
-const getTitle = (title) => title;
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello {getTitle("React")}</h1>
+      <h1>Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+
+      {/* map through the list and construct an li element for each item */}
+
+      <ul>
+        {list.map((item) => (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span> by: {item.author}</span>
+            <span>, comments ({item.num_comments})</span>
+            <span>, points ({item.points})</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
